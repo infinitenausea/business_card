d7cf31402de456fb8c739a31eb0019bb124d82c3
Anatolii Orlov <anatoly.orlov93@gmail.com>
2025-10-21 21:43:22 +0400

    projects and slider

--- js/main.js
+++ js/main.js
@@ -1,1 +1,32 @@
 new WOW().init();
+
+const swiper = new Swiper('.swiper', {
+    pagination: {
+        el: '.projects-pagination',
+        bulletClass: 'projects-bullet',
+        bulletActiveClass: 'projects-bullet-active',
+        clickable: 'true',
+    },
+});
+
+// const swiper = new Swiper('.swiper', {
+//     // Optional parameters
+//     direction: 'vertical',
+//     loop: true,
+
+//     // If we need pagination
+//     pagination: {
+//         el: '.swiper-pagination',
+//     },
+
+//     // Navigation arrows
+//     navigation: {
+//         nextEl: '.swiper-button-next',
+//         prevEl: '.swiper-button-prev',
+//     },
+
+//     // And if we need scrollbar
+//     scrollbar: {
+//         el: '.swiper-scrollbar',
+//     },
+// });
